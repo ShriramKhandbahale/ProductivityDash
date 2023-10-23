2342f8b54241bbac311228ecce3fedf94a52dabe
Shriram Khandbahale <shriramskhandbahale@gmail.com>
2023-10-23 16:13:47 +0530

    add Home Page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,37 @@
+import { NavLink } from "react-router-dom"
+import Logo from "../assets/icons/icon.png"
+
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className="Home">
+      <div className="Home__container">
+        <div className="Home__container__message">
+          <div className="Home__contianer__message__logo">
+            <img src={Logo} alt="ProductivityDash" />
+          </div>
+          <div className="Home__contianer__message__text">
+            <h1>Welcome To ProductivityDash</h1>
+          </div>
+        </div>
+        <div className="Home__container__links">
+          <div className="Home__container__links__kanban">
+            <NavLink to="/kanban">
+              <button>Kanban Board</button>
+            </NavLink>
+          </div>
+          <div className="Home__container__links__todo">
+            <NavLink to="/todo">
+              <button>Daily Todo</button>
+            </NavLink>
+          </div>
+          <div className="Home__container__links__pomodoro">
+            <NavLink to="/pomodoro">
+              <button>Pomodoro Session</button>
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
